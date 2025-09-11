d2d51fbcd41587db4fef9b04b74d4575b7097c43
HoangLeoO <leoo09012001@gmail.com>
2025-09-11 15:49:32 +0700

    bai tap oop

--- module2/src/ss4_oop/bai_tap/bai_tap2/Main.java
+++ module2/src/ss4_oop/bai_tap/bai_tap2/Main.java
@@ -4,11 +4,24 @@ public class Main {
     public static void main(String[] args) {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-
-        for (long i = 0 ; i < 10000; i++){
-            System.out.println(i);
+        int[] arrayNumber = new int[100000];
+        for (int i = 0; i < arrayNumber.length; i++) {
+            arrayNumber[i] = (int) (Math.random() * 100000);
+        }
+        for (int i = 0; i < arrayNumber.length-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < arrayNumber.length; j++) {
+                if (arrayNumber[j] < arrayNumber[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arrayNumber[i];
+            arrayNumber[i] = arrayNumber[minIndex];
+            arrayNumber[minIndex] = temp;
         }
         stopWatch.stop();
+        System.out.println(arrayNumber[0]);
+        System.out.println(arrayNumber[arrayNumber.length - 1]);
         System.out.println(stopWatch.getElapsedTime());
     }
 }
